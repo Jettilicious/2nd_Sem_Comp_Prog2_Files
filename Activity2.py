a1f48a1bddeadf4c2085c8d1e5ae398aaaf60eaf
Jettilicious <tion.jetclev@gmail.com>
2026-02-04 14:40:52 +0800

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,16 @@
+name = "Jet Clev R. Tion"
+age = 19
+height = 1.70
+isWorking = False
+
+print(f"Fullname: {name}")
+print(type(name), "\n")
+
+print(f"Age: {age}")
+print(type(age), "\n")
+
+print(f"Height in meters: {height}")
+print(type(height), "\n")
+
+print(f"Currently Working?: {isWorking}")
+print(type(isWorking), "\n")
