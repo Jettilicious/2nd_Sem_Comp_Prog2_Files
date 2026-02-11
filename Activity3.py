9a160657f56dac183c6fed30fd217a1065132003
Jettilicious <tion.jetclev@gmail.com>
2026-02-11 10:19:27 +0800

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,9 @@
+password = "GwapoKo123"
+
+print("Password: ")
+pword = input()
+
+if pword == password:
+    print("Loggin Succesful!")
+else:
+    print("Access Denied")
---
+++ ActivityInput.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+
+print(f"Good Morning {name}! It's your birthday! You are now {age} years old...")
+print(type(age))
