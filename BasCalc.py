cf0ecb1ca8bd4ba47c742ff65ddf05666f662be6
Jettilicious <tion.jetclev@gmail.com>
2026-03-07 18:42:42 +0800

    Refactor calculator operations and menu options

--- BasCalc.py
+++ BasCalc.py
@@ -1,35 +1,28 @@
 while True:
-    num1 = 0
-    num2 = 0
-    oper = ''
-    total = 0
     print(f"{'Basic Calculator':-^30}")
-    print("1. Continue")
-    print("2. Exit")
-    choice = int(input("Choice: "))
-
-    if choice == 2:
+    print("1. Add")
+    print("2. Sub")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+    oper = int(input("Choice: "))
+    
+    if oper == 5:
         print("Bye Bye!")
         break
-
     num1 = int(input("1st No.: "))
-    oper = input("Operator(+,-,*,/): ")
     num2 = int(input("2nd No.: "))
 
-    if oper == '+':
-        total = num1 + num2
-        print(f"Result: {total}")
-    elif oper == '-':
-        total = num1 - num2
-        print(f"Result: {total}")
-    elif oper == '*':
-        total = num1 * num2
-        print(f"Result: {total}")
-    elif oper == '/':
+    if oper == 1:
+        print(f"Result: {num1 + num2}")
+    elif oper == 2:
+        print(f"Result: {num1 - num2}")
+    elif oper == 3:
+        print(f"Result: {num1 * num2}")
+    elif oper == 4:
         if num2 == 0:
             print(f"{num1} cannot be divided by 0")
         else:
-            total = num1 / num2
-            print(f"Result: {total}")
+            print(f"Result: {num1 / num2}")
     else:
         print("Invalid Operator")
