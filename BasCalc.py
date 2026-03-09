c7ea44c9b677c14caf6c653a6ec661b24099861f
Jettilicious <tion.jetclev@gmail.com>
2026-03-09 20:36:15 +0800

    Enhance calculator with input validation and error handling
    Added error handling for user inputs and improved prompts.

--- BasCalc.py
+++ BasCalc.py
@@ -1,28 +1,39 @@
 while True:
-    print(f"{'Basic Calculator':-^30}")
-    print("1. Add")
-    print("2. Sub")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Exit")
-    oper = int(input("Choice: "))
+    try:
+        print(f"{'Basic Calculator':-^30}")
+        print("1. Add")
+        print("2. Sub")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+        print(f"{'':-^30}\n")
+        oper = int(input("Choice: "))
+        
+        
+        if oper == 5:
+            print("Bye Bye!")
+            break
+        if oper == 1 or oper == 2 or oper == 3 or oper == 4: 
+            try:
+                num1 = int(input("1st No.: "))
+                num2 = int(input("2nd No.: "))
+            except:
+                print("Invalid input/No input\n")
+                continue
     
-    if oper == 5:
-        print("Bye Bye!")
-        break
-    num1 = int(input("1st No.: "))
-    num2 = int(input("2nd No.: "))
-
-    if oper == 1:
-        print(f"Result: {num1 + num2}")
-    elif oper == 2:
-        print(f"Result: {num1 - num2}")
-    elif oper == 3:
-        print(f"Result: {num1 * num2}")
-    elif oper == 4:
-        if num2 == 0:
-            print(f"{num1} cannot be divided by 0")
+        if oper == 1:
+            print(f"Result: {num1 + num2}\n")
+        elif oper == 2:
+            print(f"Result: {num1 - num2}\n")
+        elif oper == 3:
+            print(f"Result: {num1 * num2}\n")
+        elif oper == 4:
+            if num2 == 0:
+                print(f"{num1} cannot be divided by 0\n")
+            else:
+                print(f"Result: {num1 / num2}\n")
         else:
-            print(f"Result: {num1 / num2}")
-    else:
-        print("Invalid Operator")
+            print("Invalid Operator\n")
+    except:
+        print("Invalid input/No input\n")
+        continue
